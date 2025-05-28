e450b8b68beaf63785b35000932051cadbff40dc
gabzzola <gazzola.co@gmail.com>
2025-05-27 23:35:55 -0300

    chore: refatorar ProjectForm para ser reutilizável

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,45 +1,23 @@
-import { useState } from 'react';
-import api from '../../services/api';
-
 import Input from '../form/Input';
 import SubmitButton from '../form/SubmitButton';
-import './ProjectForm.module.css';
+import styles from './ProjectForm.module.css';
 
-function ProjectForm() {
-
-  const [formData, setFormData] = useState({
-    name: '',
-    budget: ''
-  });
+function ProjectForm({ projectData, setProjectData, textSubmit, handleSubmit }) {
 
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
+    setProjectData({
+      ...projectData,
       [e.target.name]: e.target.value
     });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await api.post("/api/projects", formData);
-      console.log('Sucesso ao criar o projeto: ', response.data);
-
-      setFormData({ name: '', budget: '' });
-
-    } catch (error) {
-      console.error('Erro ao criar projeto: ', error);
-    }
-  };
-
   return (
-    <form onSubmit={handleSubmit} autoComplete="off">
+    <form onSubmit={handleSubmit} className={styles.form} autoComplete="off">
       <Input 
         text="Nome do Projeto"
         name="name"
         type="text"
-        value={formData.name}        
+        value={projectData.name}        
         onChange={handleChange}        
         placeholder="Insira o nome do projeto"
         required={true}        
@@ -50,13 +28,13 @@ function ProjectForm() {
         name="budget"
         type="number"
         step="0.01"
-        value={formData.budget}
+        value={projectData.budget}
         onChange={handleChange}
         placeholder="Insira o orçamento para o projeto"
         required={true}        
       />
 
-      <SubmitButton text="Criar Projeto" />
+      <SubmitButton text={textSubmit} />
     </form>
   );
 }
--- src/pages/NewProject.jsx
+++ src/pages/NewProject.jsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+import api from '../services/api';
+
 import ProjectForm from '../components/project/ProjectForm';
 import styles from './NewProject.module.css';
 
 function NewProject() {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    budget: ''
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("/api/projects", formData);
+      console.log('Sucesso ao criar o projeto: ', response.data);
+
+      setFormData({ name: '', budget: '' });
+
+    } catch (error) {
+      console.error('Erro ao criar projeto: ', error);
+    }
+  };
+
   return (
     <main className={styles.mainContainer}>
       <div className={styles.introContainer}>
@@ -9,7 +32,12 @@ function NewProject() {
         <p>Crie seu projeto para depois adicionar serviços</p>
       </div>
 
-      <ProjectForm />
+      <ProjectForm 
+        projectData={formData}
+        setProjectData={setFormData}
+        textSubmit="Criar Projeto"
+        handleSubmit={handleSubmit}
+      />
     </main>
   );
 }
