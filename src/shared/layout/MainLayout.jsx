3cd0a1d7b7474fef7d97052ec83ad83cca1d1ab5
gabzzola <gazzola.co@gmail.com>
2025-06-20 23:26:36 -0300

    feat(footer): adiciona componente Footer

--- src/shared/layout/MainLayout.jsx
+++ src/shared/layout/MainLayout.jsx
@@ -1,16 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../header/Header';
+import Footer from '../footer/Footer';
 import styles from './MainLayout.module.css';
 import sharedStyles from '../styles/SharedStyles.module.css';
 
 function MainLayout() {
   return (
-    <>
+    <div className={styles.layout}>
       <Header />
+
       <main className={`${styles.main} ${sharedStyles.container}`}>
         <Outlet />
       </main>
-    </>
+
+      <Footer />
+    </div>
   );
 }
 
