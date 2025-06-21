b7b829dc0b8375a5d34fd71a69d9b942de07b6f8
gabzzola <gazzola.co@gmail.com>
2025-06-20 22:37:48 -0300

    feat(home): criar tela Home

--- src/shared/layout/MainLayout.jsx
+++ src/shared/layout/MainLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../header/Header';
+import styles from './MainLayout.module.css';
 import sharedStyles from '../styles/SharedStyles.module.css';
 
 function MainLayout() {
   return (
     <>
       <Header />
-      <main className={sharedStyles.container}>
+      <main className={`${styles.main} ${sharedStyles.container}`}>
         <Outlet />
       </main>
     </>
